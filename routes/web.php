56f9fc70ac0b2b8dd53cc36ab1b82e78e3a112fd
syahrul-25 <pas25kata@gmail.com>
2021-08-14 14:48:09 +0900

    membuat struktur database

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::get('/timeline', TimelineController::class)->name('timeline');
+Route::middleware('auth')->group(function () {
+    Route::get('/timeline', TimelineController::class)->name('timeline');
+    Route::view('/dashboard', 'dashboard')->middleware(['auth'])->name('dashboard');
+});
 
-Route::view('/dashboard', 'dashboard')->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
